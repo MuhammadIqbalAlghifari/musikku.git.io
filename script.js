5378a88e26d241a22c141b15126b1461715010e7
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2023-08-24 19:32:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+// Nav Mobile
+const mobileNav = document.querySelector("nav.mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+
+menuIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.add("active");
+} );
+
+closeIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.remove("active");
+});
